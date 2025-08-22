021a1babb47dda99bd4bdfd66ba90d4c6f5aeafc
Matheus Ferreira <theusferreiraode@icloud.com>
2025-08-22 10:57:54 -0300

    otimização das imagens

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: { quality: 75 },
+      jpeg: { quality: 75 },
+      png: { quality: 75 },
+      webp: { quality: 75 },
+      avif: { quality: 50 },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
